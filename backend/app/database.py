c5981a46cf25804480ce3245d2b5a9784075bcf8
raulLucasGnz <raul.lucasgonzalez@gmail.com>
2025-06-14 01:11:09 +0200

    Conexión PostgreSQL y creación de modelo User en tabla de BDD

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
+#Carga variables de .env
 load_dotenv()
-
+#URL conexion a la BDD
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+#Motor de SQLAlchemy
 engine = create_engine(DATABASE_URL)
-
+#Sesiones de conexión a la BDD
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
+#Define clase base
 Base = declarative_base()
